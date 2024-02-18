42397d5cf653f4f77094a8ccb7b11cfc0f5bf0b4
Dominik <dominik.ziegenhagel@gmail.com>
2024-02-18 15:14:44 +0100

    add external image possibility

--- app.vue
+++ app.vue
@@ -1,18 +1,23 @@
 <template>
-  <div>
+    <external-image v-if="isDev" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm3JJVZNmoZ0qmayQ9ca8jzUA6LcvH7I5gZQ&usqp=CAU"/>
+  <div v-else>
     <emily-event v-if="slideIndex===0"/>
     <img v-else :src="`/bild${slideIndex}.jpg`"
          class="object-cover h-full w-full"/>
   </div>
 </template>
 <script setup>
+import ExternalImage from "~/components/external-image.vue";
+
 const slideIndex = ref(0)
 const maxSlide = 3
 const nextSlide = () => {
   slideIndex.value = (slideIndex.value + 1) % maxSlide
 }
-// if its before 20. feb, reload every 1 second
-if (new Date() < new Date('2024-02-20')) {
+
+const isDev = new Date() < new Date('2024-02-13')
+
+if (isDev) {
   setInterval(nextSlide, 1000)
 } else {
   setInterval(nextSlide, 30000)
---
+++ components/external-image.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-full w-full inset-0 fixed"
+       style="background:url('/bild2.jpg');background-size:cover;background-position:center;"></div>
+  <div class="h-full w-full inset-0 fixed"
+       :style="'background:url(' + src + '); background-size:cover;background-position:center;'"></div>
+</template>
+<script setup>
+// const tryExternalImage = computed(() => {
+//   return '/placeholder/bg' + Math.ceil(Math.random() * 5) + '.jpeg'
+// })
+
+const props = defineProps(['src'])
+</script>
