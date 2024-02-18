93bf236a103c188a2a8f583e72dea0fe4fdabc97
Dominik <dominik.ziegenhagel@gmail.com>
2024-02-18 21:18:36 +0100

    haefveir headers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: ["@nuxtjs/tailwindcss"],
+  app:{
+    //i need heavy caching in the pages header:
+    head: {
+        title: "My App",
+        meta: [
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
+            { hid: "description", name: "description", content: "My App" },
+            { hid: "cache-control", "http-equiv": "Cache-Control", content: "public, max-age=31536000, immutable" },
+            { hid: "expires", "http-equiv": "Expires", content: "31536000" },
+            { hid: "pragma", "http-equiv": "Pragma", content: "public" },
+
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    }
+  }
 })
